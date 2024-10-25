9cb426f5d5f8b1968cd4e9f5cd5b728ed7fdea2c
catharsian <zaripov.zaripov-dan@yandex.ru>
2024-10-26 00:22:43 +0300

    Cleared project for an upcoming HW2

--- src/Lib1/Library.fs
+++ src/Lib1/Library.fs
@@ -1,16 +1,10 @@
 ﻿namespace Learning
 
-module Basic =
+module Sorts =
     let swap (left: 'a byref) (right: 'a byref) =
         let temp = left
         left <- right
         right <- temp
-
-    let fib n =
-        let rec _fib p1 p2 cnt =
-            if cnt = 1 then p2 else _fib p2 (p1 + p2) (cnt - 1)
-
-        _fib 0 1 n
 
     let bubbleSort (arr: 'a array) =
         let len = arr |> Array.length
@@ -22,53 +16,6 @@ module Basic =
 
         arr
 
-    let factorial n =
-        let product = List.fold (fun st x -> x * st) 1
-        product [ 1..n ]
-
-module Matrix =
-    let product2_2 (mat1: int array2d) (mat2: int array2d) =
-        array2D
-            [| [| mat1[0, 0] * mat2[0, 0] + mat1[0, 1] * mat2[1, 0]
-                  mat1[0, 0] * mat2[0, 1] + mat1[0, 1] * mat2[1, 1] |]
-               [| mat1[1, 0] * mat2[0, 0] + mat1[1, 1] * mat2[1, 0]
-                  mat1[1, 0] * mat2[0, 1] + mat1[1, 1] * mat2[1, 1] |] |]
-
-    let sqrMat mat = product2_2 mat mat
-
-    let toBinary n =
-        let rec _f st n =
-            if n = 1 then 1 :: st else _f (n % 2 :: st) (n / 2)
-
-        _f List.empty n |> List.toArray
-
-    let Qmatrix = array2D [ [ 1; 1 ]; [ 1; 0 ] ]
-
-    let identityMatrix = array2D [ [ 1; 0 ]; [ 0; 1 ] ]
-
-    let formArrayOfMatrices n =
-        let mutable arr: (int array2d) array =
-            Array.init n (fun _ -> Array2D.init 2 2 (fun _ _ -> 0))
-
-        arr[0] <- Qmatrix
-        for i in 1 .. n - 1 do
-            arr[i] <- sqrMat arr[i - 1]
-
-        arr
-
-    let fib n =
-        let inclusion =
-            n |> toBinary |> Array.rev |> Array.map System.Convert.ToBoolean
-
-        let array_of_mat = inclusion.Length |> formArrayOfMatrices
-
-        let _folder st mat incl = if incl then product2_2 st mat else st
-
-        let final_matrix = Array.fold2 _folder identityMatrix array_of_mat inclusion
-
-        final_matrix[0, 1] // 0,0 is next fib; 0,1 and 1,0 is current fib; 1,1 is previous fib
-
-module QuickSort =
     let quickSort (arr: 'a array) =
         let partition (arr: 'a array) low high =
             let pivot = arr[high]
@@ -79,9 +26,9 @@ module QuickSort =
                 if arr[j] < pivot then
                     i <- i + 1
 
-                    Basic.swap &arr[i] &arr[j]
+                    swap &arr[i] &arr[j]
 
-            Basic.swap &arr[i + 1] &arr[high]
+            swap &arr[i + 1] &arr[high]
             arr, i + 1
 
         let rec _quickSort (arr: 'a array) low high =
--- test/Lib1Tests/Tests.fs
+++ test/Lib1Tests/Tests.fs
@@ -7,60 +7,6 @@ open Microsoft.VisualStudio.TestTools.UnitTesting
 
 [<TestClass>]
 type TestClass() =
-    [<TestMethod>]
-    member this.TestFactorial() =
-        let expected = [ 1; 2; 6; 24; 120; 720 ]
-        let actual = [ 1..6 ] |> List.map Learning.Basic.factorial
-
-        Assert.AreEqual(expected, actual)
-
-    [<TestMethod>]
-    member this.TestBasicFib() =
-        let expected = [ 1; 1; 2; 3; 5; 8; 13; 21; 34; 55; 89 ]
-        let actual = [ 1..11 ] |> List.map Learning.Basic.fib
-
-        Assert.AreEqual(expected, actual)
-
-    [<TestMethod>]
-    member this.TestMatrixProduct() =
-        let expected =
-            array2D[[ 19; 22 ]
-                    [ 43; 50 ]]
-
-        let mat1 = array2D [ [ 1; 2 ]; [ 3; 4 ] ]
-        let mat2 = array2D [ [ 5; 6 ]; [ 7; 8 ] ]
-        let actual = Learning.Matrix.product2_2 mat1 mat2
-
-        CollectionAssert.AreEqual(actual, expected)
-
-    [<TestMethod>]
-    member this.TestToBinary() =
-        let expected =
-            [ [| 1; 1; 1; 1; 1 |]; [| 1 |]; [| 1; 1 |]; [| 1; 1; 0 |]; [| 1; 0; 0; 0; 0 |] ]
-
-        let actual =
-            List.map Learning.Matrix.toBinary [ 16 + 8 + 4 + 2 + 1; 1; 1 + 2; 2 + 4; 16 ]
-
-        (expected, actual) ||> List.zip |> List.iter CollectionAssert.AreEqual
-
-
-    [<TestMethod>]
-    member this.TestMatrixFibFunction() =
-        for i in 1..10 do
-            let expected = Learning.Basic.fib i
-            let actual = Learning.Matrix.fib i
-
-            Assert.AreEqual(expected, actual)
-
-    [<TestMethod>]
-    member this.TestSwapFunction() =
-        let arr = [| 1; 2; 3; 4; 5; 6; 10; 8; 9; 7 |]
-        let expected = [| 1..10 |]
-        let actual = arr
-        Learning.Basic.swap &actual[6] &actual[9]
-
-        CollectionAssert.AreEqual(expected, actual)
-
     member this.CheckSortingAlgorithm arr algorithm =
         let expected = arr |> Array.sort
         let actual = arr |> algorithm
@@ -76,9 +22,9 @@ type TestClass() =
     [<TestMethod>]
     member this.TestBubbleSort() =
         for _ in 1..50 do
-            this.CheckSortingAlgorithm (this.GenerateRandomArray()) Learning.Basic.bubbleSort
+            this.CheckSortingAlgorithm (this.GenerateRandomArray()) Learning.Sorts.bubbleSort
 
     [<TestMethod>]
     member this.TestQuickSort() =
         for _ in 1..50 do
-            this.CheckSortingAlgorithm (this.GenerateRandomArray()) Learning.QuickSort.quickSort
+            this.CheckSortingAlgorithm (this.GenerateRandomArray()) Learning.Sorts.quickSort
