635711a6ec9cd1e15bc1d1ac2bc789fd6d2c124b
Danil-Zaripov <zaripov.zaripov-dan@yandex.ru>
2024-11-05 20:18:28 +0300

    Homework 2(30.09) (#2)
    * Implemented Merge sort.
    
    * Generalized sorting algorithms.
    
    * Made sorting functions functional(input doesn't change environment). Changed the tests accordingly.
    
    * Added tests for empty arrays

--- src/Lib1/Library.fs
+++ src/Lib1/Library.fs
@@ -6,24 +6,32 @@ module Sorts =
         left <- right
         right <- temp
 
-    let bubbleSort (arr: 'a array) =
-        let len = arr |> Array.length
+    let ascendingOrderCompare a b = a < b
+    let descendingOrderCompare a b = b < a
+
+    let bubbleSortGeneral (arr: 'a array) compare =
+        let arr = arr |> Array.copy
+
+        let len = arr.Length
 
         for i in 0 .. len - 1 do
             for j in 1 .. len - 1 do
-                if arr[j] < arr[j - 1] then
-                    swap &arr[j] &arr[j - 1]
+                if compare arr[j] arr[j - 1] then
+                    swap &arr[j - 1] &arr[j]
 
         arr
 
-    let quickSort (arr: 'a array) =
+    let quickSortGeneral (arr: 'a array) compare =
+        let arr = arr |> Array.copy
+
         let partition (arr: 'a array) low high =
             let pivot = arr[high]
 
             let mutable i = low - 1
 
             for j in low .. high - 1 do
-                if arr[j] < pivot then
+                if compare arr[j] pivot then
+
                     i <- i + 1
 
                     swap &arr[i] &arr[j]
@@ -41,4 +49,46 @@ module Sorts =
                 arr
 
         _quickSort arr 0 (arr.Length - 1)
-    
+
+    let divide_two_halves (left, right) =
+        let length = right - left + 1 // end-inclusive
+        let half_length = length / 2
+
+        (left, left + half_length - 1), (left + half_length, right)
+
+    let mergeSortGeneral arr compare =
+        let arr = arr |> Array.copy
+
+        let _merge (arr1: 'a array) (arr2: 'a array) =
+            let final = Array.zeroCreate (arr1.Length + arr2.Length)
+            let mutable fp = 0
+            let mutable sp = 0
+
+            for i in 0 .. final.Length - 1 do
+                if fp >= arr1.Length then
+                    final[i] <- arr2[sp]
+                    sp <- sp + 1
+                elif sp >= arr2.Length then
+                    final[i] <- arr1[fp]
+                    fp <- fp + 1
+                elif compare arr1[fp] arr2[sp] then
+                    final[i] <- arr1[fp]
+                    fp <- fp + 1
+                else
+                    final[i] <- arr2[sp]
+                    sp <- sp + 1
+
+            final
+
+        let rec _mergeSort (arr: 'a array) =
+            if arr.Length <= 1 then
+                arr
+            else
+                let (f1, f2), (s1, s2) = divide_two_halves (0, arr.Length - 1)
+                _merge (_mergeSort arr[f1..f2]) (_mergeSort arr[s1..s2])
+
+        _mergeSort arr
+
+    let ascendingSort (sort: 'a array -> ('a -> 'a -> bool) -> 'a array) arr = ascendingOrderCompare |> sort arr
+
+    let descendingSort (sort: 'a array -> ('a -> 'a -> bool) -> 'a array) arr = descendingOrderCompare |> sort arr
--- test/Lib1Tests/Tests.fs
+++ test/Lib1Tests/Tests.fs
@@ -2,14 +2,20 @@ namespace LearningTests
 
 open System
 open Microsoft.VisualStudio.TestTools.UnitTesting
-
+open Learning
 
 
 [<TestClass>]
 type TestClass() =
-    member this.CheckSortingAlgorithm arr algorithm =
+    member this.CheckSortingAlgorithmAscending arr algorithm =
         let expected = arr |> Array.sort
-        let actual = arr |> algorithm
+        let actual = arr |> (Sorts.ascendingSort algorithm)
+
+        CollectionAssert.AreEqual(expected, actual)
+
+    member this.CheckSortingAlgorithmDescending arr algorithm =
+        let expected = arr |> Array.sort |> Array.rev
+        let actual = arr |> (Sorts.descendingSort algorithm)
 
         CollectionAssert.AreEqual(expected, actual)
 
@@ -20,11 +26,61 @@ type TestClass() =
                rand.Next(-100, 100) |]
 
     [<TestMethod>]
-    member this.TestBubbleSort() =
-        for _ in 1..50 do
-            this.CheckSortingAlgorithm (this.GenerateRandomArray()) Learning.Sorts.bubbleSort
+    member this.DivideFunctionTest() =
+        let tests = [ (0, 3); (0, 1); (1, 3); (1, 4) ]
+
+        let expected =
+            [ ((0, 1), (2, 3)); ((0, 0), (1, 1)); ((1, 1), (2, 3)); ((1, 2), (3, 4)) ]
+
+        let actual = tests |> List.map Sorts.divide_two_halves
+
+        Assert.AreEqual(expected, actual)
+
+
+    member this.TestSortingAlgorithmEmpty alg =
+        let expected = [||]
+        let actual = [||] |> (Sorts.ascendingSort Sorts.bubbleSortGeneral)
+
+        CollectionAssert.AreEqual(expected, actual)
 
     [<TestMethod>]
-    member this.TestQuickSort() =
+    member this.CheckBubbleSortEmpty() =
+        this.TestSortingAlgorithmEmpty Sorts.bubbleSortGeneral
+
+    [<TestMethod>]
+    member this.CheckQuickSortEmpty() =
+        this.TestSortingAlgorithmEmpty Sorts.quickSortGeneral
+
+    [<TestMethod>]
+    member this.CheckMergeSortEmpty() =
+        this.TestSortingAlgorithmEmpty Sorts.mergeSortGeneral
+
+    [<TestMethod>]
+    member this.TestBubbleSortAscending() =
         for _ in 1..50 do
-            this.CheckSortingAlgorithm (this.GenerateRandomArray()) Learning.Sorts.quickSort
+            this.CheckSortingAlgorithmAscending (this.GenerateRandomArray()) Sorts.bubbleSortGeneral
+
+    [<TestMethod>]
+    member this.TestQuickSortAscending() =
+        for _ in 1..50 do
+            this.CheckSortingAlgorithmAscending (this.GenerateRandomArray()) Sorts.quickSortGeneral
+
+    [<TestMethod>]
+    member this.TestMergeSortAscending() =
+        for _ in 1..50 do
+            this.CheckSortingAlgorithmAscending (this.GenerateRandomArray()) Sorts.mergeSortGeneral
+
+    [<TestMethod>]
+    member this.TestBubbleSortDescending() =
+        for _ in 1..50 do
+            this.CheckSortingAlgorithmDescending (this.GenerateRandomArray()) Sorts.bubbleSortGeneral
+
+    [<TestMethod>]
+    member this.TestQuickSortDescending() =
+        for _ in 1..50 do
+            this.CheckSortingAlgorithmDescending (this.GenerateRandomArray()) Sorts.quickSortGeneral
+
+    [<TestMethod>]
+    member this.TestMergeSortDescending() =
+        for _ in 1..50 do
+            this.CheckSortingAlgorithmDescending (this.GenerateRandomArray()) Sorts.mergeSortGeneral
