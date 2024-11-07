b3788f476b4e76249171671227406b40b332a749
catharsian <zaripov.zaripov-dan@yandex.ru>
2024-11-07 17:55:56 +0300

    Implemented list sorting. Added property based testing.

--- src/Lib1/Library.fs
+++ src/Lib1/Library.fs
@@ -1,2 +1,44 @@
 ﻿namespace Learning
 
+module ListSorts =
+    let bubbleSort lst =
+        let rec _bubbleSort =
+            function
+            | p1 :: p2 :: rest ->
+                if p1 > p2 then
+                    p2 :: (_bubbleSort (p1 :: rest))
+                else
+                    p1 :: (_bubbleSort (p2 :: rest))
+            | rest -> rest
+
+        List.fold (fun acc _ -> _bubbleSort acc) lst lst
+
+    let rec quickSort =
+        function
+        | [] -> []
+        | x :: xs ->
+            let smaller = List.filter ((>) x) xs
+            let bigger = List.filter ((<=) x) xs
+            (quickSort smaller) @ [ x ] @ (quickSort bigger)
+
+    let rec mergeSort lst =
+        let rec _merge =
+            function
+            | (x :: xs, y :: ys) ->
+                if x < y then
+                    x :: (_merge (xs, y :: ys))
+                else
+                    y :: (_merge (x :: xs, ys))
+            | ([], y :: ys) -> y :: ys
+            | (x :: xs, []) -> x :: xs
+            | ([], []) -> []
+
+        match lst with
+        | p1 :: p2 :: rest ->
+            let l1, l2 =
+                let chunks = lst |> List.splitInto 2
+                let p1, p2 = (chunks |> List.head, chunks |> List.tail |> List.exactlyOne)
+                p1, p2
+
+            _merge (mergeSort l1, mergeSort l2)
+        | _ -> lst
