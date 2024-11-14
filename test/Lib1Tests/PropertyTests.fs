956ae8288998d7ab39d15054d3c487f71af64868
catharsian <zaripov.zaripov-dan@yandex.ru>
2024-11-14 23:07:05 +0300

    Initial HW4 commit

--- test/Lib1Tests/PropertyTests.fs
+++ test/Lib1Tests/PropertyTests.fs
@@ -4,16 +4,5 @@ open FsCheck
 open FsCheck.Xunit
 
 [<Properties(MaxTest = 100)>]
-module SortTests =
-
-    [<Property>]
-    let CheckBubbleSortAlgorithm (lst: int list) =
-        (lst |> Learning.ListSorts.bubbleSort) = (lst |> List.sort)
-
-    [<Property>]
-    let CheckQuickSortAlgorithm (lst: int list) =
-        (lst |> Learning.ListSorts.quickSort) = (lst |> List.sort)
-
-    [<Property>]
-    let CheckMergeSortAlgorithm (lst: int list) =
-        (lst |> Learning.ListSorts.mergeSort) = (lst |> List.sort)
+module SortTests =  
+    let placeholder() = ()
