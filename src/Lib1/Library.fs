b84966641aee17634d0ecc5d0b12185d927ebd94
catharsian <zaripov.zaripov-dan@yandex.ru>
2024-11-14 23:07:05 +0300

    Initial HW4 commit

--- src/Lib1/Library.fs
+++ src/Lib1/Library.fs
@@ -40,3 +40,85 @@ module ListSorts =
 
             _merge (mergeSort l1, mergeSort l2)
         | _ -> lst
+
+module ArraySorts = 
+    let swap (left: 'a byref) (right: 'a byref) =
+        let temp = left
+        left <- right
+        right <- temp
+
+    let bubbleSort (arr: 'a array) =
+        let len = arr |> Array.length
+
+        for i in 0 .. len - 1 do
+            for j in 1 .. len - 1 do
+                if arr[j] < arr[j - 1] then
+                    swap &arr[j] &arr[j - 1]
+
+        arr
+
+    let quickSort (arr: 'a array) =
+        let partition (arr: 'a array) low high =
+            let pivot = arr[high]
+
+            let mutable i = low - 1
+
+            for j in low .. high - 1 do
+                if arr[j] < pivot then
+                    i <- i + 1
+
+                    swap &arr[i] &arr[j]
+
+            swap &arr[i + 1] &arr[high]
+            arr, i + 1
+
+        let rec _quickSort (arr: 'a array) low high =
+            if low < high then
+                let (arr, piv) = partition arr low high
+                let arr = _quickSort arr low (piv - 1)
+                let arr = _quickSort arr (piv + 1) high
+                arr
+            else
+                arr
+
+        _quickSort arr 0 (arr.Length - 1)
+
+    
+    let divide_two_halves (left, right) =
+        let length = right - left + 1 // end-inclusive
+        let half_length = length / 2
+
+        (left, left + half_length - 1), (left + half_length, right)
+    
+    let mergeSort arr =
+        let arr = arr |> Array.copy
+
+        let _merge (arr1: 'a array) (arr2: 'a array) =
+            let final = Array.zeroCreate (arr1.Length + arr2.Length)
+            let mutable fp = 0
+            let mutable sp = 0
+
+            for i in 0 .. final.Length - 1 do
+                if fp >= arr1.Length then
+                    final[i] <- arr2[sp]
+                    sp <- sp + 1
+                elif sp >= arr2.Length then
+                    final[i] <- arr1[fp]
+                    fp <- fp + 1
+                elif arr1[fp] < arr2[sp] then
+                    final[i] <- arr1[fp]
+                    fp <- fp + 1
+                else
+                    final[i] <- arr2[sp]
+                    sp <- sp + 1
+
+            final
+
+        let rec _mergeSort (arr: 'a array) =
+            if arr.Length <= 1 then
+                arr
+            else
+                let (f1, f2), (s1, s2) = divide_two_halves (0, arr.Length - 1)
+                _merge (_mergeSort arr[f1..f2]) (_mergeSort arr[s1..s2])
+
+        _mergeSort arr
