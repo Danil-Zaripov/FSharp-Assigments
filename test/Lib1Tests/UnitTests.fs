19773fb122269e837ab4db43dc073bf7c8dce9bb
catharsian <zaripov.zaripov-dan@yandex.ru>
2024-11-14 22:36:02 +0300

    Added unit tests for delicate cases

--- test/Lib1Tests/UnitTests.fs
+++ test/Lib1Tests/UnitTests.fs
@@ -3,4 +3,21 @@
 open Xunit
 
 module UnitTests =
-    let placeholder () = ()
+    let testCases = [ []; [ 1; 2; 3; 4; 5 ]; [ 5; 4; 3; 2; 1 ] ]
+
+    let checkForTestCases alg =
+        let algorithmWorks xs = (List.sort xs) = (alg xs)
+
+        testCases |> List.map algorithmWorks |> List.reduce (&&)
+
+    [<Fact>]
+    let ``bubbleSort works for delicate cases`` () =
+        Assert.True(checkForTestCases Learning.ListSorts.bubbleSort)
+
+    [<Fact>]
+    let ``quickSort works for delicate cases`` () =
+        Assert.True(checkForTestCases Learning.ListSorts.quickSort)
+
+    [<Fact>]
+    let ``mergeSort works for delicate cases`` () =
+        Assert.True(checkForTestCases Learning.ListSorts.mergeSort)
