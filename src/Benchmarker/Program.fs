031ae82a28d38992f16f2c124cbf0dfffcc180b2
catharsian <zaripov.zaripov-dan@yandex.ru>
2024-12-04 17:54:01 +0300

    Benchmark functions completely revisioned. Getting ready for plotting.

--- src/Benchmarker/Program.fs
+++ src/Benchmarker/Program.fs
@@ -1,76 +1,123 @@
 ﻿module Program
 
 open System
-open System.Threading
+open System.Timers
 open Learning
 open BenchmarkDotNet.Attributes
 open BenchmarkDotNet.Running
+open BenchmarkDotNet.Configs
+open BenchmarkDotNet.Reports
+open Perfolizer.Horology
+open Perfolizer.Metrology
+open BenchmarkDotNet.Exporters
+open System.Globalization
+open System.IO
+
+let bigDataElemCount = [ 50000..10000..150000 ]
+let smallDataElemCount = [ 5000..1000..15000 ]
 
 
-[<MemoryDiagnoser>]
-[<SimpleJob(launchCount = 1, iterationCount = 1, warmupCount = 1)>]
-type ListSortBenchmarking() =
+type BigDataArrayAndListSortComparisonBenchmark() =
     let rand = Random()
 
-    member public this._elemCounts = [| 10000..10000..100000 |]
+    member public this._elemCounts = bigDataElemCount
 
     [<ParamsSource("_elemCounts")>]
-    member val elemCount = 0 with get, set
+    member val Element_Count = 0 with get, set
 
-    member val data = [ 1 ] with get, set
+    member val dataList = [ 1 ] with get, set
+    member val dataArray = [| 1 |] with get, set
+
 
     [<IterationSetup>]
     member this.initData() =
-        this.data <- [ 1 .. this.elemCount ] |> List.map (fun _ -> rand.Next(-100000, 100000))
-
-
-    //[<Benchmark>]
-    //member this.bubbleSort() = ListSorts.bubbleSort this.data
+        this.dataList <- [ 1 .. this.Element_Count ] |> List.map (fun _ -> rand.Next(-100000, 100000))
+        this.dataArray <- [| 1 .. this.Element_Count |] |> Array.map (fun _ -> rand.Next(-100000, 100000))
 
     [<Benchmark>]
-    member this.quickSort() = ListSorts.quickSort this.data
+    member this.List_Quick_Sort() = ListSorts.quickSort this.dataList
 
     [<Benchmark>]
-    member this.mergeSort() = ListSorts.mergeSort this.data
+    member this.List_Merge_Sort() = ListSorts.mergeSort this.dataList
 
-    [<Benchmark(Baseline = true)>]
-    member this.systemSort() = List.sort this.data
+    [<Benchmark>]
+    member this.Array_Quick_Sort() = ArraySorts.quickSort this.dataArray
 
-[<MemoryDiagnoser>]
-[<SimpleJob(launchCount = 1, iterationCount = 1, warmupCount = 1)>]
-type ArraySortBenchmarking() =
+    [<Benchmark>]
+    member this.Array_Merge_Sort() = ArraySorts.mergeSort this.dataArray
+
+    [<Benchmark(Description = "List.sort")>]
+    member this.ListSort() = List.sort this.dataList
+
+    [<Benchmark(Baseline = true, Description = "Array.sort")>]
+    member this.ArraySort() = Array.sort this.dataArray
+
+type SmallDataListAndArraySortComparisonBenchmark() =
     let rand = Random()
 
-    member public this._elemCounts = [| 10000..10000..20000 |]
+    member public this._elemCounts = smallDataElemCount
 
     [<ParamsSource("_elemCounts")>]
-    member val elemCount = 0 with get, set
+    member val Element_Count = 0 with get, set
 
-    member val data = [| 1 |] with get, set
+    member val dataList = [ 1 ] with get, set
+    member val dataArray = [| 1 |] with get, set
+
 
     [<IterationSetup>]
     member this.initData() =
-        this.data <- [| 1 .. this.elemCount |] |> Array.map (fun _ -> rand.Next(-100000, 100000))
-
-    //[<Benchmark>]
-    //member this.bubbleSort() = ArraySorts.bubbleSort this.data
+        this.dataList <- [ 1 .. this.Element_Count ] |> List.map (fun _ -> rand.Next(-100000, 100000))
+        this.dataArray <- [| 1 .. this.Element_Count |] |> Array.map (fun _ -> rand.Next(-100000, 100000))
 
     [<Benchmark>]
-    member this.quickSort() = ArraySorts.quickSort this.data
+    member this.List_Bubble_Sort() = ListSorts.bubbleSort this.dataList
 
     [<Benchmark>]
-    member this.mergeSort() = ArraySorts.mergeSort this.data
+    member this.List_Insertion_Sort() = ListSorts.insertionSort this.dataList
 
     [<Benchmark(Baseline = true)>]
-    member this.systemSort() = Array.sort this.data
+    member this.Array_Bubble_Sort() = ArraySorts.bubbleSort this.dataArray
+
+    [<Benchmark>]
+    member this.Array_Insertion_Sort() = ArraySorts.insertionSort this.dataArray
+
 
 [<EntryPoint>]
 let main args =
-    //let summary = BenchmarkRunner.Run<ArraySortBenchmarking>()
+    let tablesPath = "./src/PythonPlotting/Tables/"
+    let artifactsPath = "./BenchmarkDotNet.Artifacts/"
+
+
+    let style =
+        new SummaryStyle(
+            cultureInfo = CultureInfo.InvariantCulture,
+            printUnitsInHeader = true,
+            printUnitsInContent = false,
+            timeUnit = TimeUnit.Millisecond, // Seconds for slow sorts // Milliseconds for fast sorts
+            sizeUnit = SizeUnit.MB
+        )
+
+    let exporter = Csv.CsvExporter(Csv.CsvSeparator.Semicolon, style)
+
+    let config =
+        ManualConfig
+            .Create(DefaultConfig.Instance)
+            .WithSummaryStyle(style)
+            .AddExporter(exporter)
+
 
     let switcher =
-        BenchmarkSwitcher [| typeof<ListSortBenchmarking>; typeof<ArraySortBenchmarking> |]
+        BenchmarkSwitcher
+            [| typeof<BigDataArrayAndListSortComparisonBenchmark>
+               typeof<SmallDataListAndArraySortComparisonBenchmark> |]
 
-    switcher.Run args |> ignore
+    let summaries = switcher.Run(args, config)
+
+    for summary in summaries do
+        let title = summary.Title
+        let filename = title.Split('-')[0]
+        let filepath = $"{artifactsPath}results/{filename}-report.csv"
+
+        File.Copy(filepath, $"{tablesPath}{filename}.csv", true)
 
     0
--- test/Lib1Tests/PropertyTests.fs
+++ test/Lib1Tests/PropertyTests.fs
@@ -5,4 +5,10 @@ open FsCheck.Xunit
 
 [<Properties(MaxTest = 100)>]
 module SortTests =
-    let placeholder () = ()
+    [<Property>]
+    let CheckListInsertionSort (xs: int list) =
+        (List.sort xs) = (Learning.ListSorts.insertionSort xs)
+
+    [<Property>]
+    let CheckArrayInsertionSort (arr: int array) =
+        System.Linq.Enumerable.SequenceEqual(Array.sort arr, Learning.ArraySorts.insertionSort arr)
