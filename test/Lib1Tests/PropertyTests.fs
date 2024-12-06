4fdee7e5e28a655da19368ee64cf5235a173be29
catharsian <zaripov.zaripov-dan@yandex.ru>
2024-12-06 23:31:54 +0300

    Main prerequisite initial commit.

---
+++ test/Lib1Tests/PropertyTests.fs
@@ -0,0 +1,8 @@
+﻿namespace Tests
+
+open FsCheck
+open FsCheck.Xunit
+
+[<Properties(MaxTest = 100)>]
+module PropertyTests =
+    let placeholder () = ()
---
+++ test/Lib1Tests/UnitTests.fs
@@ -0,0 +1,6 @@
+﻿namespace Tests
+
+open Xunit
+
+module UnitTests =
+    let placeholder () = ()
