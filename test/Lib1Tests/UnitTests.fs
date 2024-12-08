68061093fe0220c7cef753a37a96936427fc22fd
catharsian <zaripov.zaripov-dan@yandex.ru>
2024-12-08 15:47:57 +0300

    Implemented tree.

--- test/Lib1Tests/UnitTests.fs
+++ test/Lib1Tests/UnitTests.fs
@@ -1,6 +1,30 @@
 ﻿namespace Tests
 
 open Xunit
+open Trees
 
 module UnitTests =
-    let placeholder () = ()
+    let tree =
+        Node(
+            [ Node([ Leaf 3; Node [ Leaf 5; Leaf 7; Leaf 20 ]; Leaf 1 ])
+              Node([ Leaf 3; Leaf 5; Node([ Leaf 8 ]) ]) ]
+        )
+
+    [<Fact>]
+    let checkSumFold () =
+        let expected = 3 + 5 + 7 + 20 + 1 + 3 + 5 + 8
+        let actual = MyTree.fold (+) 0 tree
+
+        Assert.Equal(expected, actual)
+
+    [<Fact>]
+    let checkMap () =
+        let expected = (3 + 5 + 7 + 20 + 1 + 3 + 5 + 8) * 2
+        let actual = tree |> MyTree.map ((*) 2) |> MyTree.fold (+) 0
+        Assert.Equal(expected, actual)
+
+    [<Fact>]
+    let checkHeight () =
+        let expected = 4
+        let actual = MyTree.height tree
+        Assert.Equal(expected, actual)
