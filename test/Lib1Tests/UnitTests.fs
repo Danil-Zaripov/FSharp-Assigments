d0742960811061567aaf5d18e7c7d5d60928826d
catharsian <zaripov.zaripov-dan@yandex.ru>
2024-12-24 10:05:39 +0300

    Completed HW5.

--- test/Lib1Tests/UnitTests.fs
+++ test/Lib1Tests/UnitTests.fs
@@ -1,6 +1,44 @@
 ﻿namespace Tests
 
 open Xunit
+open ImageProcessing
+
 
 module UnitTests =
-    let placeholder () = ()
+    let imagesFolder = "../../../../../Images/"
+    let samplePath = imagesFolder + "Koshak.png"
+
+    let checkFilter filename filter =
+        let path = imagesFolder + filename
+        let expected = loadAs2DArray path
+
+        let actual = loadAs2DArray samplePath |> applyFilter filter
+        Assert.Equal(expected, actual)
+
+    [<Fact>]
+    let checkGaussianBlur () =
+        checkFilter "Koshak-gaussblur.png" gaussianBlurKernel
+
+    [<Fact>]
+    let checkEdges () =
+        checkFilter "Koshak-edges.png" edgesKernel
+
+    [<Fact>]
+    let checkUnsharpMasking () =
+        checkFilter "Koshak-unsharpMasking.png" unsharpMaskingKernel
+
+    [<Fact>]
+    let checkRidge () =
+        checkFilter "Koshak-ridge.png" ridgeKernel
+
+    [<Fact>]
+    let checkTopSobel () =
+        checkFilter "Koshak-topSobel.png" topSobelKernel
+
+    [<Fact>]
+    let checkOutline () =
+        checkFilter "Koshak-outline.png" outlineKernel
+
+    [<Fact>]
+    let checkSharpen () =
+        checkFilter "Koshak-sharpen.png" sharpenKernel
